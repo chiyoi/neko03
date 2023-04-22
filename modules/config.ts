8dc98d222c08befa7f58d3ba42594b7dd824f10e
chiyoi <chiyoi2140@outlook.com>
2023-04-22 16:41:10 +0800

    update

--- modules/config.ts
+++ modules/config.ts
@@ -3,9 +3,13 @@ import Constants from "expo-constants"
 export const config = {
   ClientIDAzureADApplication: "e5a68652-2fed-4508-ad85-02e7a966307f",
 
-  EndpointService: () => Constants.manifest?.extra?.ENV === "prod" ? (
+  EndpointService: () => isProd() ? (
     "https://api.neko03.moe/"
   ) : (
     "http://silver.local/"
   ),
+}
+
+export function isProd() {
+  return Constants.manifest?.extra?.ENV === "prod"
 }
