6c767c4473fba983ecb6c67010f66649281dee1e
chiyoi <chiyoi2140@outlook.com>
2023-08-22 05:20:40 +0000

    reset

--- applications/neko03/modules/toast.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react"
-
-export const ToastContext = createContext<(toast: string) => void>(() => void 0)
-
-export type IDString = {
-  id: number,
-  str: string,
-}
