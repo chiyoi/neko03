e4adcc74858be9d08951cf5520400c0e82d12e56
chiyoi <chiyoi2140@outlook.com>
2023-12-14 23:38:26 +0800

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,11 @@ import '@radix-ui/themes/styles.css'
 export const metadata: Metadata = {
   title: 'Neko03',
   description: 'にゃん〜',
+  icons: {
+    shortcut: 'https://neko03.moe/favicon.icl',
+    icon: 'https://neko03.moe/favicon.ico',
+    apple: 'https://neko03.moe/favicon.ico',
+  }
 }
 
 export const viewport: Viewport = {
