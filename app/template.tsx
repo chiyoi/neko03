158680f4504114b5de570761f026de7d487bd7f5
chiyoi <chiyoi2140@outlook.com>
2023-12-26 00:01:54 +0800

    tidy

--- app/template.tsx
+++ app/template.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Box, Flex, Theme } from "@radix-ui/themes"
+import { Flex, Theme } from "@radix-ui/themes"
 import { ThemeProvider } from "next-themes"
 
 export default function Template({ children }: { children: React.ReactNode }) {
